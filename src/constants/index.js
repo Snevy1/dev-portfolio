5c939d02b3629089cc2beaecce4a01a51d34b305
Snevy1 <simiyunevily@gmail.com>
2025-03-13 07:20:01 +0300

    Resolved link clicking issue

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,11 +58,16 @@ export const clientReviews = [
 
 export const myProjects = [
     {
-        title: 'Podcastr - AI Podcast Platform',
-        desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+        title: 'Telewing - Healthcare Management system',
+        desc: 'A full-stack Hospital Management System designed to streamline healthcare operations, enhance \n' +
+            'patient management, and improve hospital workflow efficiency',
         subdesc:
-            'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-        href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+            'Built using the MERN stack (MySQL, \n' +
+            'Express.js, React, Node.js) with a secure JWT-based authentication system and role-based access \n' +
+            'control (RBAC).',
+        githubUrl: "https://github.com/Snevy1/hospital-management-system",
+        href: "https://telewing.netlify.app",
+
         texture: '/textures/project/project1.mp4',
         logo: '/assets/project-logo1.png',
         logoStyle: {
@@ -95,11 +100,12 @@ export const myProjects = [
         ],
     },
     {
-        title: 'LiveDoc - Real-Time Google Docs Clone',
-        desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+        title: 'Hoolichat',
+        desc: 'A social media chatting application, used by medical students: • Dynamic and interactive sections built with React. • Fast messaging and notifications using Socket.io • State management using Zustand • Minimalistic and visually appealing design using Tailwind CSS. • Easy navigation and responsive layout for an enhanced user experience. • Deployed with Vercel for professional presentation.',
         subdesc:
             'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-        href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+        githubUrl: "https://github.com/Snevy1/hospital-management-system",
+        href: "https://telewing.netlify.app",
         texture: '/textures/project/project2.mp4',
         logo: '/assets/project-logo2.png',
         logoStyle: {
@@ -132,11 +138,12 @@ export const myProjects = [
         ],
     },
     {
-        title: 'CarePulse - Health Management System',
-        desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+        title: 'Inventory Management system',
+        desc: 'This is a fullstack inventory management system built with nextjs15, prisma, mongodb and shadcn.It helps management of inventory as seemless as possible',
         subdesc:
             'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
-        href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+        githubUrl: "https://github.com/Snevy1/hospital-management-system",
+        href: 'https://inventory-tau-two.vercel.app/',
         texture: '/textures/project/project3.mp4',
         logo: '/assets/project-logo3.png',
         logoStyle: {
@@ -171,11 +178,12 @@ export const myProjects = [
         ],
     },
     {
-        title: 'Horizon - Online Banking Platform',
-        desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+        title: 'Seti - Educational Website',
+        desc: 'I used Flexbox and grid css to arrange things. Javascript was mainly used to control interactivity. I also used swiperjs for testimonials.',
         subdesc:
             'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-        href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+        githubUrl: "https://github.com/Snevy1/Educational-website",
+        href: 'https://snevy1.github.io/Educational-website/about.html',
         texture: '/textures/project/project4.mp4',
         logo: '/assets/project-logo4.png',
         logoStyle: {
@@ -208,11 +216,12 @@ export const myProjects = [
         ],
     },
     {
-        title: 'Imaginify - AI Photo Manipulation App',
-        desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+        title: 'Hoolichat - A social Media application used by Medical students',
+        desc: 'A clone of the Zentry website created to demonstrate proficiency in frontend development.',
         subdesc:
             'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-        href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+            githubUrl: "https://github.com/Snevy1/zentry-clone",
+            href: 'https://zentry-clone-dun.vercel.app/',
         texture: '/textures/project/project5.mp4',
         logo: '/assets/project-logo5.png',
         logoStyle: {
@@ -260,17 +269,17 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
     {
         id: 1,
-        name: 'Framer',
+        name: 'Plotperty',
         pos: 'Lead Web Developer',
         duration: '2022 - Present',
-        title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+        title: "Plotperty serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
         icon: '/assets/framer.svg',
         animation: 'victory',
     },
     {
         id: 2,
-        name: 'Figma',
-        pos: 'Web Developer',
+        name: 'Ploperty',
+        pos: 'Software developer Intern',
         duration: '2020 - 2022',
         title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
         icon: '/assets/figma.svg',
@@ -278,8 +287,8 @@ export const workExperiences = [
     },
     {
         id: 3,
-        name: 'Notion',
-        pos: 'Junior Web Developer',
+        name: 'Upwork',
+        pos: 'Junior Web Developer-Freelance',
         duration: '2019 - 2020',
         title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
         icon: '/assets/notion.svg',
